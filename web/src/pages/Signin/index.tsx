63d086244254856dc329eae859eb8fca6b8eac04
Caio Torres <caiocftorres@gmail.com>
2020-11-15 22:19:44 -0300

    creating login page

---
+++ web/src/pages/Signin/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logoImg.svg';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu logon</h1>
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="ha">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
