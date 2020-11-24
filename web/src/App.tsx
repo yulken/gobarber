09b27299734f256ca1af553f18b40eed55b4ab56
Caio Torres <caiocftorres@gmail.com>
2020-11-23 22:05:23 -0300

    toast structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,14 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { AuthProvider } from './hooks/AuthContext';
 import GlobalStyle from './styles/global';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
