74ca4eccf1bac9fa8684ffa263a7f72bb2b6fb5b
Caio Torres <caiocftorres@gmail.com>
2020-12-03 07:33:45 -0300

    configuring navigation

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Container } from './styles';
+
+const SignIn: React.FC = () => <Container />;
+export default SignIn;
---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => <Container />;
+export default SignUp;
