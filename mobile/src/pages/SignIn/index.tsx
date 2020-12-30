cd0e9349e37b18d979846f60c3f273a51ff3d36b
Caio Torres <caiocftorres@gmail.com>
2020-12-30 09:01:34 -0300

    Finishing auth view

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import { Image } from 'react-native';
-import { Container, Title } from './styles';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-    <Title>Faça seu logon</Title>
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
+const SignIn: React.FC = () => {
+  const [isVisible, setVisible] = useState(true);
 
-    <Button
-      onPress={() => {
-        console.log('dasd');
-      }}
-    >
-      Entrar
-    </Button>
-  </Container>
-);
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => setVisible(false));
+    Keyboard.addListener('keyboardDidHide', () => setVisible(true));
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <Title>Faça seu logon</Title>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { }}>Entrar</Button>
+            <ForgotPassword onPress={() => { }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      {isVisible && (
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
+  );
+};
+
 export default SignIn;
