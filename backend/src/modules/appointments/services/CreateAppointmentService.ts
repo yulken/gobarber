c548f55afc8c58f10d1844ac53a2b9f9f1eb75cb
Caio Torres <caiocftorres@gmail.com>
2021-01-20 19:33:10 -0300

    refactor to include Liskov subst principle

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,7 +2,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentRepository';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentRepository';
 
 interface RequestDTO {
   provider_id: string;
