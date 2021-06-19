40b46f5f20acf8bd7624b1f304ecdbec9d042594
Caio Torres <caiocftorres@gmail.com>
2021-06-19 10:18:32 -0300

    explaing coverage bug

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -64,6 +64,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
+      //set to false for 100% bnranch coverage
       isolatedModules: true
     }
   },
