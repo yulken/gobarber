d73469e7faa4d06133f2a0518d193956f7dfb5e2
Caio Torres <caiocftorres@gmail.com>
2021-06-19 22:14:37 -0300

    creating update profile feature / test

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import uploadConfig from '@config/upload';
+import multer from 'multer';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import UsersController from '../controllers/UsersController';
@@ -9,8 +12,14 @@ const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
+const upload = multer(uploadConfig);
+
 usersRouter.post('/', usersController.create);
 
-usersRouter.patch('/avatar', ensureAuthenticated, userAvatarController.update);
-
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
 export default usersRouter;
