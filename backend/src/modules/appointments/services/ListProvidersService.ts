ca30a21dc0f01fc6527d023a9e3bc6ca05c073cd
Caio Torres <caiocftorres@gmail.com>
2021-08-16 18:08:44 -0300

    listing providers by cache

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 interface IRequest {
@@ -12,11 +13,23 @@ export default class ShowProfileService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    return this.usersRepository.findAllProviders({
-      id_exception: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        id_exception: user_id,
+      });
+      await this.cacheProvider.save<User[]>(`providers-list:${user_id}`, users);
+    }
+
+    return users;
   }
 }
